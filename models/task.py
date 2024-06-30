9655d15b347c021d5935617f27ffef5951f0019b
Alves0611 <g.alves0611@gmail.com>
2024-06-30 15:26:15 -0300

    fix: correct Task model to_dict method to properly include title

--- models/task.py
+++ models/task.py
@@ -8,7 +8,7 @@ class Task:
     def to_dict(self):
         return {
             "id": self.id,
-            "title": self.id,
+            "title": self.title,
             "description": self.description,
             "completed": self.completed,
         }
