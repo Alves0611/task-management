e53da5afee7ea1f181c55c3937b12371013732c6
Alves0611 <g.alves0611@gmail.com>
2024-06-30 15:07:17 -0300

    feat: add endpoint to retrieve all tasks

--- app.py
+++ app.py
@@ -22,5 +22,12 @@ def create_task():
     return jsonify({"message": "Successful task list"})
 
 
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    output = {"tasks": task_list, "total_tasks": len(task_list)}
+    return jsonify(output)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
